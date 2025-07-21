77d7815890d77d1145e1fe264ab446c26129d6f0
Christian Larrea <57969291+BlackSkyFrost@users.noreply.github.com>
2025-07-21 14:45:54 -0500

    Overwrite views for login

--- fastlib/views.py
+++ fastlib/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from .forms import FormLoginPersonalizado, FormRegistroPersonalizado
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -11,23 +11,23 @@ def inicio(request):
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = FormLoginPersonalizado(request, data=request.POST)
         if form.is_valid():
             usuario = form.get_user()
             login(request, usuario)
             return redirect('inicio')
     else:
-        form = AuthenticationForm()
+        form = FormLoginPersonalizado()
     return render(request, 'fastlib/login.html', {'form': form})
 
 def registro_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = FormRegistroPersonalizado(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = FormRegistroPersonalizado()
     return render(request, 'fastlib/registro.html', {'form': form})
 
 def logout_view(request):
