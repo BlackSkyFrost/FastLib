3fba211bcf69a7e236bec102abd5e18578b8cd15
Christian Larrea <57969291+BlackSkyFrost@users.noreply.github.com>
2025-07-21 12:13:29 -0500

    View and model for dummy data

--- fastlib/models.py
+++ fastlib/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Tarea(models.Model):
+    titulo = models.CharField(max_length=100)
+    completado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo
