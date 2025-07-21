41c143ea3ec7a86f350b87eb984629a6fb3f793b
Christian Larrea <57969291+BlackSkyFrost@users.noreply.github.com>
2025-07-21 12:26:12 -0500

    Login & Register views for dummy data

--- fastlib/views.py
+++ fastlib/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required
 def inicio(request):
-    return HttpResponse("¡Hola desde mi primera vista Django!")
+    return render(request, 'fastlib/inicio.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            usuario = form.get_user()
+            login(request, usuario)
+            return redirect('inicio')
+    else:
+        form = AuthenticationForm()
+    return redirect(request, 'fastlib/login.html', {'form': form})
+
+def registro_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'fastlib/registro.html', {'form': form})
+
