7235efdab1959792986e467d84c3666ada2af829
Christian Larrea <57969291+BlackSkyFrost@users.noreply.github.com>
2025-07-21 17:01:00 -0500

    urls updated

--- fastlib/urls.py
+++ fastlib/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('login/', views.login_view, name='login'),
     path('registro/', views.registro_view, name='registro'),
     path('logout/', views.logout_view, name='logout'),
+]
+urlpatterns += [
+    path('editar-perfil/', views.editar_perfil_view, name='editar_perfil'),
+    path('cambiar-contraseña/', auth_views.PasswordChangeView.as_view(
+        template_name='mi_aplicacion/cambiar_contraseña.html',
+        success_url='/'
+    ), name='cambiar_contraseña'),
 ]
